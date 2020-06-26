e0212c72b3293b4d70500b04a2fffb5acdceac84
umapalanivel <umapalanivel@gmail.com>
2020-06-26 21:25:39 +0530

    Created Initial Xcode project

---
+++ CountryDetails/CountryDetails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryDetails
+//
+//  Created by uma palanivel on 26/06/20.
+//  Copyright © 2020 umapalanivel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
