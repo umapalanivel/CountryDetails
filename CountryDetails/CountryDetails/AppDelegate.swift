44e03d923563dd828f895833d3c21f2d77863b72
umapalanivel <umapalanivel@gmail.com>
2020-06-26 21:57:19 +0530

    AppDelegate Changes

--- CountryDetails/CountryDetails/AppDelegate.swift
+++ CountryDetails/CountryDetails/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame:UIScreen.main.bounds)
+        if let windowScreen  = window {
+            windowScreen.rootViewController = ViewController()
+            windowScreen.backgroundColor = UIColor.white
+            windowScreen.makeKeyAndVisible()
+        }
         return true
     }
 
